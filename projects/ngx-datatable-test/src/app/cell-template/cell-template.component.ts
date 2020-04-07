bd15bc3c156fb02148287511359d38653475e929
Tushar Ghosh <tushar.ghosh@bjitgroup.com>
2020-04-07 09:52:34 +0600

    Added 100k data in demo

--- projects/ngx-datatable-test/src/app/cell-template/cell-template.component.ts
+++ projects/ngx-datatable-test/src/app/cell-template/cell-template.component.ts
@@ -8,7 +8,8 @@ import { AppService } from '../app.service';
 
 export class CellTemplateComponent implements OnInit  {
     @ViewChild('actionTpl', { static: true }) actionTpl: TemplateRef<any>;
-    options = {}
+    @ViewChild('addressTpl', { static: true }) addressTpl: TemplateRef<any>;
+    options = {};
     data = [];
     columns: any = {};
     columnsWithFeatures:any
@@ -21,18 +22,19 @@ export class CellTemplateComponent implements OnInit  {
         this.columns = [
             { key: 'id', title: '<div class="blue"><i class="fa fa-id-card-o"></i> ID</div>',  width: 60, sorting: true },
             { key: 'name', title: '<div class="blue"><i class="fa fa-user"></i> Name</div>', width: 100 },
-            { key: 'phone', title: '<div class="blue"><i class="fa fa-phone"></i> Phone</div>', align: { head: 'left' }, width: 100, sorting: true },
-            { key: 'company', title: '<div class="blue"><i class="fa fa-building"></i>  Company</div>', width: 300, sorting: true, align: { head: 'left', body: 'right' }, noWrap: { head: true, body: true } },
-            { key: 'date', title: '<div class="blue"><i class="fa fa-calendar-times-o"></i> Date</div>', sorting: false},
+            { key: 'gender', title: '<div class="blue"><i class="fa fa-phone"></i> Gender</div>', align: { head: 'left' }, width: 100, sorting: true },
+            { key: 'address', title: '<div class="blue"><i class="fa fa-building"></i>  Company</div>', width: 300, sorting: true, align: { head: 'left', body: 'right' }, noWrap: { head: true, body: true }, cellTemplate: this.addressTpl },
+            { key: 'age', title: '<div class="blue"><i class="fa fa-calendar-times-o"></i> Date</div>', sorting: false},
             { key: 'zip', title: '<div class="blue">Action</div>', align: { head: 'center', body:  'center' }, sorting: false, width: 80, cellTemplate: this.actionTpl }
         ]
         setTimeout(() => {
-            this.data = this.appService.getData();
+            this.data = this.appService.get100KData();
             this.options = {
                 ...this.options,
                 loader : false
             }
         }, 2000);
+        console.log(this.appService.get100KData())
        
     }
 
